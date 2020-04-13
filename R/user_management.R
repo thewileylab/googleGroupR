f7e652da3ed2aeb4faf51557f8d61fdf0724345a
David Mayer <david.mayer@cuanschutz.edu>
2020-04-13 14:08:51 -0600

    Add documentation to user_management functions - use rlang and .data for explicit variables - add dependencies

--- R/user_management.R
+++ R/user_management.R
@@ -1,18 +1,35 @@
+#' Add User
+#'
+#' Add a user to a Google Group
+#'
+#' @param domain A GSuite domain: 'somedomain.org'
+#' @param group The name of a Google Group. A listing of groups in your domain can be obtained by running list_groups(domain = 'mydomain.org')
+#' @param email The Gmail Account of the user that you would like to add to the group.
+#'
+#' @return
+#' @export
+#' @importFrom rlang .data
+#' @importFrom magrittr %>% extract2
+#' @importFrom tibble enframe
+#' @importFrom dplyr mutate filter pull
+#' @importFrom purrr map_chr
+#' @importFrom glue glue
+#' @importFrom httr add_headers content POST
 add_user <- function(domain, group, email) {
   groups <- list_groups(domain) %>%
     magrittr::extract2('groups') %>%
     tibble::enframe(name = NULL, value = 'group_info') %>%
-    dplyr::mutate(group_name = purrr::map_chr(.x = group_info,
+    dplyr::mutate(group_name = purrr::map_chr(.x = .data$group_info,
                                                ~ .x %>%
                                                  magrittr::extract2('name')
                                                ),
-                  group_id = purrr::map_chr(.x = group_info,
+                  group_id = purrr::map_chr(.x = .data$group_info,
                                             ~ .x %>%
                                               magrittr::extract2('id'))
                   )
   group_id <- groups %>%
-    dplyr::filter(group_name == group) %>%
-    pull(group_id)
+    dplyr::filter(.data$group_name == group) %>%
+    dplyr::pull(.data$group_id)
   access_token <- get_access_token()
   add_user_url <- glue('https://www.googleapis.com/admin/directory/v1/groups/{group_id}/members')
   add_user_header <- httr::add_headers('Content-Type' = 'application/json',
@@ -22,21 +39,38 @@ add_user <- function(domain, group, email) {
   httr::content(httr::POST(url = add_user_url, add_user_header, body = add_user_body, encode='json'))
 }
 
+#' Remove User
+#'
+#' Remove a user from a Google Group
+#'
+#' @param domain A GSuite domain: 'somedomain.org'
+#' @param group The name of a Google Group. A listing of groups in your domain can be obtained by running list_groups(domain = 'mydomain.org')
+#' @param email The Gmail Account of the user that you would like to add to the group.
+#'
+#' @return
+#' @export
+#' @importFrom rlang .data
+#' @importFrom magrittr %>% extract2
+#' @importFrom tibble enframe
+#' @importFrom dplyr mutate filter pull
+#' @importFrom purrr map_chr
+#' @importFrom glue glue
+#' @importFrom httr add_headers DELETE
 remove_user <- function(domain, group, email) {
   groups <- list_groups(domain) %>%
     magrittr::extract2('groups') %>%
     tibble::enframe(name = NULL, value = 'group_info') %>%
-    dplyr::mutate(group_name = purrr::map_chr(.x = group_info,
+    dplyr::mutate(group_name = purrr::map_chr(.x = .data$group_info,
                                               ~ .x %>%
                                                 magrittr::extract2('name')
-    ),
-    group_id = purrr::map_chr(.x = group_info,
+                                              ),
+                  group_id = purrr::map_chr(.x = .data$group_info,
                               ~ .x %>%
                                 magrittr::extract2('id'))
-    )
+                  )
   group_id <- groups %>%
-    dplyr::filter(group_name == group) %>%
-    pull(group_id)
+    dplyr::filter(.data$group_name == group) %>%
+    dplyr::pull(.data$group_id)
   access_token <- get_access_token()
   remove_user_url <- glue('https://www.googleapis.com/admin/directory/v1/groups/{group_id}/members/{email}')
   remove_user_header <- httr::add_headers('Authorization' = glue('Bearer {access_token}'),
