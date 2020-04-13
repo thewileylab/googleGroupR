15a9b2629ba7ea8aa29886b385e97b3c511a384f
David Mayer <david.mayer@cuanschutz.edu>
2020-04-13 09:50:48 -0600

    ensure domain is specified when listing groups

--- R/list_groups.R
+++ R/list_groups.R
@@ -9,9 +9,13 @@
 #' @importFrom glue glue
 #' @importFrom httr add_headers content GET
 list_groups <- function(domain) {
+  if (missing(domain) ) {
+    message('Please specify a GSuite domain.')
+  } else {
   access_token <- get_access_token()
   auth_header <- httr::add_headers('Authorization' = glue('Bearer {access_token}'))
   httr::content(httr::GET(glue::glue('https://www.googleapis.com/admin/directory/v1/groups/?domain={domain}'),
                           auth_header)
   )
+  }
 }
