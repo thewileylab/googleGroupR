544cafc6853920018a064255abe6a2a789951b2e
David Mayer <david.mayer@cuanschutz.edu>
2020-04-14 11:57:12 -0600

    list_group_members - list all memebers within a group

---
+++ R/list_group_members.R
@@ -0,0 +1,23 @@
+#' List Group Members
+#'
+#' Retrieves a paginated list of all members in a group.
+#'
+#' @param domain A GSuite domain: 'somedomain.org'
+#' @param group The name of a Google Group. A listing of groups in your domain can be obtained by running list_groups(domain = 'mydomain.org')#'
+#'
+#' @return
+#' @export
+#' @importFrom glue glue
+#' @importFrom httr add_headers content GET
+list_group_members <- function (domain, group) {
+  if (missing(group) ) {
+    message('Please specify a GSuite domain.')
+  } else {
+    group_id <- get_group_id(domain, group)
+    access_token <- get_access_token()
+    auth_header <- httr::add_headers('Authorization' = glue('Bearer {access_token}'))
+    httr::content(httr::GET(glue::glue('https://www.googleapis.com/admin/directory/v1/groups/{group_id}/members'),
+                            auth_header)
+    )
+  }
+}
