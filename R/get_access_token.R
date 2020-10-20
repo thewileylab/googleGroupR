6249c933d86e8f7f15a066ac0c3a101a5aa8108c
David Mayer <david.mayer@cuanschutz.edu>
2020-10-20 10:06:42 -0600

    remove old Google clientID - rely on internal application assets - updated cached credential location

--- R/get_access_token.R
+++ R/get_access_token.R
@@ -1,3 +1,25 @@
+# Helpers ----
+#' Installed App
+#'
+#' Invisibly returns an OAuth app
+#'
+#' @return An Invisible OAuth consumer application, produced by [httr::oauth_app()]
+#'
+#' @export
+#' @keywords internal
+#' @rdname internal-assets
+installed_app <- function() {
+  ggoa()
+}
+#' @export
+#' @keywords internal
+#' @rdname internal-assets
+#' @noRd
+print.hidden_fn <- function(x, ...) {
+  x <- 'Nope'
+  NextMethod('print')
+}
+
 #' Get Access Token
 #'
 #' @param cached_credentials Location to read from/store credentials.
@@ -9,13 +31,14 @@
 #' @importFrom glue glue
 #' @importFrom jsonlite fromJSON
 #' @importFrom httr content GET add_headers POST
-get_access_token <- function(cached_credentials = '~/.adminSDK_cache.rds') {
-  # OAuth 2.0 Client ID
-  secrets <- jsonlite::fromJSON(txt = system.file('extdata/OAuth_ClientID/client_secret.json', package = 'googleGroupR'))
-  # Scopes
-  scopes <- "https://www.googleapis.com/auth/admin.directory.group https://www.googleapis.com/auth/apps.groups.settings"
-  # Create Authorization Link
-  auth_link <- glue::glue('https://accounts.google.com/o/oauth2/auth?client_id={secrets$installed$client_id}&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope={scopes}&response_type=code')
+get_access_token <- function(cached_credentials = '~/.googleGroupR_cache.rds') {
+  # OAuth 2.0 Info
+  ## Token URI
+    token_uri <- "https://oauth2.googleapis.com/token"
+  ## Scopes
+    scopes <- "https://www.googleapis.com/auth/admin.directory.group https://www.googleapis.com/auth/apps.groups.settings"
+  ## Authorization Link
+    auth_link <- glue::glue('https://accounts.google.com/o/oauth2/auth?client_id={installed_app()$key}&redirect_uri={installed_app()$redirect_uri}&scope={scopes}&response_type=code')
 
   if(file.exists(cached_credentials) ) {
     cached_token <- readRDS(cached_credentials)
@@ -23,11 +46,11 @@ get_access_token <- function(cached_credentials = '~/.adminSDK_cache.rds') {
       message(glue::glue('Please visit the following URL to generate an authorization code: {auth_link}'))
       auth_code <- readline(prompt =  glue::glue('Enter authorization code:'))
       access_token_body <- list(code=auth_code,
-                                client_id=secrets$installed$client_id,
-                                client_secret=secrets$installed$client_secret,
-                                redirect_uri='urn:ietf:wg:oauth:2.0:oob',
+                                client_id=installed_app()$key,
+                                client_secret=installed_app()$secret,
+                                redirect_uri=installed_app()$redirect_uri,
                                 grant_type='authorization_code')
-      token <- httr::content(httr::POST(url = secrets$installed$token_uri, body = access_token_body))
+      token <- httr::content(httr::POST(url = token_uri, body = access_token_body))
       saveRDS(object = token, file = cached_credentials)
       token$access_token
     } else {
@@ -43,9 +66,9 @@ get_access_token <- function(cached_credentials = '~/.adminSDK_cache.rds') {
           refresh_header <- httr::add_headers('Content-Type' = 'application/x-www-form-urlencoded')
           refresh_body <- list(grant_type='refresh_token',
                                refresh_token=cached_token$refresh_token,
-                               client_id=secrets$installed$client_id,
-                               client_secret=secrets$installed$client_secret)
-          token <- httr::content(httr::POST(url = secrets$installed$token_uri, refresh_header, body = refresh_body, encode='form'))
+                               client_id=installed_app()$key,
+                               client_secret=installed_app()$secret)
+          token <- httr::content(httr::POST(url = token_uri, refresh_header, body = refresh_body, encode='form'))
           cached_token$access_token <- token$access_token
           saveRDS(object = cached_token, file = cached_credentials)
           token$access_token
@@ -56,9 +79,9 @@ get_access_token <- function(cached_credentials = '~/.adminSDK_cache.rds') {
         refresh_header <- httr::add_headers('Content-Type' = 'application/x-www-form-urlencoded')
         refresh_body <- list(grant_type='refresh_token',
                              refresh_token=cached_token$refresh_token,
-                             client_id=secrets$installed$client_id,
-                             client_secret=secrets$installed$client_secret)
-        token <- httr::content(httr::POST(url = secrets$installed$token_uri, refresh_header, body = refresh_body, encode='form'))
+                             client_id=installed_app()$key,
+                             client_secret=installed_app()$secret)
+        token <- httr::content(httr::POST(url = token_uri, refresh_header, body = refresh_body, encode='form'))
         cached_token$access_token <- token$access_token
         saveRDS(object = cached_token, file = cached_credentials)
         token$access_token
@@ -70,11 +93,11 @@ get_access_token <- function(cached_credentials = '~/.adminSDK_cache.rds') {
     message(glue::glue('Please visit the following URL to generate an authorization code: {auth_link}'))
     auth_code <- readline(prompt =  glue::glue('Enter authorization code:'))
     access_token_body <- list(code=auth_code,
-                              client_id=secrets$installed$client_id,
-                              client_secret=secrets$installed$client_secret,
-                              redirect_uri='urn:ietf:wg:oauth:2.0:oob',
+                              client_id=installed_app()$key,
+                              client_secret=installed_app()$secret,
+                              redirect_uri=installed_app()$redirect_uri,
                               grant_type='authorization_code')
-    token <- httr::content(httr::POST(url = secrets$installed$token_uri, body = access_token_body))
+    token <- httr::content(httr::POST(url = token_uri, body = access_token_body))
     saveRDS(object = token, file = cached_credentials)
     token$access_token
   }
