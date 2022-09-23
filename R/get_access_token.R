882244b435b202645d42fa4761ef857ed90f82b2
David Mayer <david.mayer@cuanschutz.edu>
2022-09-23 09:19:53 -0600

    add additional flow for when access_token is no longer available

--- R/get_access_token.R
+++ R/get_access_token.R
@@ -59,14 +59,37 @@ get_access_token <- function(cached_credentials = '~/.config/googleGroupR/.googl
       token <- httr::content(httr::POST(url = token_uri, body = access_token_body))
       saveRDS(object = token, file = cached_credentials)
       token$access_token
-      } else {
-        google_oauth_url <- 'https://accounts.google.com/o/oauth2/'
-        token_info <- httr::content(httr::GET(glue::glue('{google_oauth_url}tokeninfo?access_token={cached_token$access_token}')))
-        if('expires_in' %in% names(token_info) ){
-          if(token_info$expires_in > 0 ) {
-            message('Using access token from cache.')
-            cached_token$access_token
-            } else {
+      } else if (!'access_token' %in% names(cached_token)) {
+        auth_code <- oauth_listener(auth_link, is_interactive = interactive())
+        access_token_body <- list(code=auth_code$code,
+                                  client_id=installed_app()$key,
+                                  client_secret=installed_app()$secret,
+                                  redirect_uri=installed_app()$redirect_uri,
+                                  grant_type='authorization_code')
+        token <- httr::content(httr::POST(url = token_uri, body = access_token_body))
+        saveRDS(object = token, file = cached_credentials)
+        token$access_token
+        } else {
+          google_oauth_url <- 'https://accounts.google.com/o/oauth2/'
+          token_info <- httr::content(httr::GET(glue::glue('{google_oauth_url}tokeninfo?access_token={cached_token$access_token}')))
+          if('expires_in' %in% names(token_info) ){
+            if(token_info$expires_in > 0 ) {
+              message('Using access token from cache.')
+              cached_token$access_token
+              } else {
+                message('Cached token expired, obtaining new access token.')
+                # Create Refresh cURL command pieces
+                refresh_header <- httr::add_headers('Content-Type' = 'application/x-www-form-urlencoded')
+                refresh_body <- list(grant_type='refresh_token',
+                                     refresh_token=cached_token$refresh_token,
+                                     client_id=installed_app()$key,
+                                     client_secret=installed_app()$secret)
+                token <- httr::content(httr::POST(url = token_uri, refresh_header, body = refresh_body, encode='form'))
+                cached_token$access_token <- token$access_token
+                saveRDS(object = cached_token, file = cached_credentials)
+                token$access_token
+                }
+            } else if ('error' %in% names(token_info) ){
               message('Cached token expired, obtaining new access token.')
               # Create Refresh cURL command pieces
               refresh_header <- httr::add_headers('Content-Type' = 'application/x-www-form-urlencoded')
@@ -78,23 +101,10 @@ get_access_token <- function(cached_credentials = '~/.config/googleGroupR/.googl
               cached_token$access_token <- token$access_token
               saveRDS(object = cached_token, file = cached_credentials)
               token$access_token
-              }
-          } else if ('error' %in% names(token_info) ){
-            message('Cached token expired, obtaining new access token.')
-            # Create Refresh cURL command pieces
-            refresh_header <- httr::add_headers('Content-Type' = 'application/x-www-form-urlencoded')
-            refresh_body <- list(grant_type='refresh_token',
-                                 refresh_token=cached_token$refresh_token,
-                                 client_id=installed_app()$key,
-                                 client_secret=installed_app()$secret)
-            token <- httr::content(httr::POST(url = token_uri, refresh_header, body = refresh_body, encode='form'))
-            cached_token$access_token <- token$access_token
-            saveRDS(object = cached_token, file = cached_credentials)
-            token$access_token
-            } else {
-              message('Not sure how to handle current token situation. Go ask an adult.')
-              }
-        }
+              } else {
+                message('Not sure how to handle current token situation. Go ask an adult.')
+                }
+          }
     } else {
       # message(glue::glue('Please visit the following URL to generate an authorization code: {auth_link}'))
       # auth_code <- readline(prompt =  glue::glue('Enter authorization code:'))
